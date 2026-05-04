80c74baf2ea1d2e164fd9afe61ccf0dc71d065d8
ruthram123 <ruthramsocialmedia@gmail.com>
2026-05-04 10:14:08 +0530

    04-05

--- 3dvista.js
+++ 3dvista.js
@@ -216,7 +216,7 @@
       hoverBorder: "rgba(46, 139, 87, 0.2)",
       onClick: () => {
         DVPopup.close();
-        openIframePopup("https://www.google.com/maps/embed?pb=!4v1773894128625!6m8!1m7!1sCAoSF0NJSE0wb2dLRUlDQWdJQ3A2dHVnNGdF!2m2!1d10.29539444450159!2d78.76225519795156!3f185.9805093672628!4f0.6916854146053169!5f0.7820865974627469");
+        openIframePopup("https://www.google.com/maps/embed?pb=!4v1777869334299!6m8!1m7!1sCAoSHENJQUJJaERIX2U3amszOGhfX2dCZW5iWjhFeFc.!2m2!1d10.2953534686256!2d78.7622327153955!3f193.4346635054857!4f-1.1351482729987623!5f0.7820865974627469");
       },
     });
     body.appendChild(gsvBtn);
